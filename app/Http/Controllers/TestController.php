7adbafa6cb994b30b285fed3c1be601479c7eea6
Alex <lg280683man1@yandex.ru>
2023-12-10 18:03:05 +0300

    создание проекта

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function index()
+    {
+        $a = 1;
+        $b = 2;
+        $c = $a + $b;
+
+        dd($c);
+    }
+}
